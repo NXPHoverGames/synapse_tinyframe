652712da50843fcc360b7126cb0de58795b2bf48
James Goppert <james.goppert@gmail.com>
2023-03-25 10:00:17 -0400

    Add synapse topics.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

---
+++ include/synapse_tinyframe/SynapseTopics.h
@@ -0,0 +1,18 @@
+#ifndef SynapseTopicsH
+#define SynapseTopicsH
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SYNAPSE_TOPIC_CMD_VEL 0x01
+#define SYNAPSE_TOPIC_ODOMETRY_EXTERNAL 0x02
+#define SYNAPSE_TOPIC_JOY 0x03
+#define SYNAPSE_TOPIC_BEZIER 0x04
+#define SYNAPSE_TOPIC_ACTUATORS 0x05
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //SynapseTopicsH
